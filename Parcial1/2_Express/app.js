a479ed59fa7d14c1fba4eba81dbc26606bf3d0f8
NoraRdz <narl031003@gmail.com>
2025-08-29 07:37:10 -0500

    Se subio la tarea del servidor con middleware con validador de content-type

--- Parcial1/2_Express/app.js
+++ Parcial1/2_Express/app.js
@@ -3,9 +3,35 @@ const app = express();
 
 const port = 3000
 
+function middlewareValidador(req, res, next) {
+
+    const contentType = req.headers["content-type"];
+    
+    if (!contentType || !contentType.includes("application/json")) {
+      return res.status(415).json({ 
+        error: "El Content-Type debe ser application/json" 
+      });
+    }
+
+  next();
+}
+
+
 app.get('/', (req,res)=>{
-    res.send("Hola mundo")
-})
+    res.json({"message":"Hola mundo"})
+});
+
+app.post('/',middlewareValidador, (req,res)=>{
+   res.json({"message":"Hola mundo"})
+});
+
+app.put('/', (req,res)=>{
+    res.json({"message":"Hola mundo"})
+});
+
+app.delete('/', (req,res)=>{
+    res.json({"message":"Hola mundo"})
+});
 
 
 app.listen(port, ()=>{
