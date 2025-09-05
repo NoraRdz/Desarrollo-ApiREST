9c63346ef232d6329aa22a3a9790aaff4d00a71e
NoraRdz <narl031003@gmail.com>
2025-09-04 23:43:43 -0500

    Se subio la actividad de CORS

---
+++ Parcial1/4_ExpressCORS/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = 3000
+
+const clientes = require('./router/clientes');
+const proveedores = require('./router/proveedores');
+
+app.use(cors())
+
+app.use('/clientes',clientes.router)
+app.use('/proveedores',proveedores.router)
+
+
+app.listen(port, ()=>{
+    console.log("server corriendo en http://localhost:3000")
+})
