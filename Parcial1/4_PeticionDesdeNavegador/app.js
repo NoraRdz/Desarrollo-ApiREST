ca2aeec7148c8a0a0e5fc1cfd057ca29950ec04e
NoraRdz <narl031003@gmail.com>
2025-09-10 08:23:17 -0500

    Se subio la actividad morgan

--- Parcial1/4_PeticionDesdeNavegador/app.js
+++ Parcial1/4_PeticionDesdeNavegador/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 
 const port = 3000
 
 const clientes = require('./router/clientes');
 const proveedores = require('./router/proveedores');
-
-app.use(cors())
+app.use(morgan('tiny'))
+// app.use(cors())
 
 app.use('/clientes',clientes.router)
 app.use('/proveedores',proveedores.router)
